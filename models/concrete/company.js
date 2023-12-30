a470c5cc5fdaffb3a8a3071eb6435cad9f8fe2ae
Eyup Cakli <eyupcakli@hotmail.com>
2023-12-30 11:31:28 +0300

    feat: company collection created.

---
+++ models/concrete/company.js
@@ -0,0 +1,21 @@
+const { default: mongoose } = require("mongoose");
+const mongoos = require("mongoose");
+
+const companySchema = new mongoos.Schema({
+    name: {
+        type: String,
+        require: [ true, 'Company name should be declared.'],
+        unique: [ true, 'This company name already registered.']
+    },
+    restaurantType: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    }
+},{timestamps: true});
+
+const company = mongoose.model('company', companySchema);
+module.exports = company;
