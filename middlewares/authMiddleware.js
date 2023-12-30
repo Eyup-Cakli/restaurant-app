f35fdbe5816580ec0a556ed03c52a3f947f105f0
Eyup Cakli <eyupcakli@hotmail.com>
2023-12-30 14:25:07 +0300

    feat: authMiddleware created.

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const userModel = require("../models/concrete/userModel.js");
+
+const publicKey = fs.readFileSync('./certs/public.pem');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    // check json web token exists & is verified
+    if (token) {
+        jwt.verify(token, publicKey, (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect('/login');
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+}
+
+// check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, publicKey, async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await userModel.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
