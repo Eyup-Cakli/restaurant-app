daf9c95512662f5c987fbc4c7d5f97fd7c10f01a
Eyup Cakli <eyupcakli@hotmail.com>
2023-12-30 14:19:18 +0300

    feat: certs, config and lib layers created.

---
+++ config/connectMongoDB.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const dbURI = "mongodb+srv://eyupcakli98:atlas123@restaurant-app.m2hycjs.mongodb.net/";
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  };
+
+  module.exports.connectMongoDB = () => {
+    mongoose
+      .connect(dbURI, options)
+      .then(() => console.log("Database connected."))
+      .catch((err) => console.error("Database connection error: ", err));
+  };
